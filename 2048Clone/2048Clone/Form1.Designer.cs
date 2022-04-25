b7706570ba0687104eafffbf42380c837c1ae524
IneedtoGitaJob <84819013+IneedtoGitaJob@users.noreply.github.com>
2022-04-24 20:42:35 -0400

    Add files via upload

---
+++ 2048Clone/2048Clone/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace _2048Clone
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Location = new System.Drawing.Point(234, 669);
+            this.buttonUp.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(112, 35);
+            this.buttonUp.TabIndex = 0;
+            this.buttonUp.Text = "up";
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonUp_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Location = new System.Drawing.Point(114, 714);
+            this.buttonLeft.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(112, 35);
+            this.buttonLeft.TabIndex = 1;
+            this.buttonLeft.Text = "left";
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonLeft_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Location = new System.Drawing.Point(354, 714);
+            this.buttonRight.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(112, 35);
+            this.buttonRight.TabIndex = 2;
+            this.buttonRight.Text = "right";
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonRight_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Location = new System.Drawing.Point(234, 714);
+            this.buttonDown.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(112, 35);
+            this.buttonDown.TabIndex = 3;
+            this.buttonDown.Text = "down";
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonDown_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ControlText;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 150;
+            this.dataGridView1.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.Size = new System.Drawing.Size(600, 600);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 786);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonUp);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ 2048Clone/2048Clone/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2048Clone
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            dataGridView1.ColumnHeadersVisible = false;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.Font = new Font("Tahoma", 40);
+            dataGridView1.RowCount = 4;
+            dataGridView1.ColumnCount = 4;
+            dataGridView1.RowTemplate.Height = dataGridView1.Height / 4;
+
+            //Initialize to 0
+            for (int x = 0; x < 4; x++)
+            {
+
+                dataGridView1.Rows[x].SetValues(0, 0, 0, 0);
+            }
+
+
+            //Create and seed random
+            Random rand = new Random((int)DateTime.Now.Ticks);
+            for (int x = 0; x < rand.Next(2, 5); x++)
+            {
+                dataGridView1.Rows[rand.Next(0, 4)].Cells[rand.Next(0, 4)].Value = 2;
+
+            }
+
+
+        }
+
+        private void buttonUp_Click(object sender, EventArgs e)
+        {
+            upShift(0, 3);
+            newValue();
+        }
+
+        private void buttonLeft_Click(object sender, EventArgs e)
+        {
+            leftShift(3, 0);
+            newValue();
+        }
+
+        private void buttonRight_Click(object sender, EventArgs e)
+        {
+            rightShift(0,0);
+            newValue();
+        }
+
+        private void buttonDown_Click(object sender, EventArgs e)
+        {
+            downShift(0, 0);
+            newValue();
+        }
+
+        private void leftShift(int column, int row)
+        {
+            
+            for (int y = row; y < 4; y++)
+            {
+                
+                for (int x = column; x > 0; x--)
+                {
+                    
+
+
+                    if (((int)dataGridView1.Rows[y].Cells[x - 1].Value == (int)dataGridView1.Rows[y].Cells[x].Value) || ((int)dataGridView1.Rows[y].Cells[x - 1].Value * (int)dataGridView1.Rows[y].Cells[x].Value == 0))
+                    {
+                        dataGridView1.Rows[y].Cells[x - 1].Value = (int)dataGridView1.Rows[y].Cells[x].Value + (int)dataGridView1.Rows[y].Cells[x - 1].Value;
+                        dataGridView1.Rows[y].Cells[x].Value = 0;
+                    }
+
+
+                }
+
+
+            }
+        }
+
+        private void upShift(int column, int row)
+        {
+            
+            for (int y = row; y > 0; y--)
+            {
+                
+                for (int x = column; x < 4; x++)
+                {
+                    
+                    if (((int)dataGridView1.Rows[y - 1].Cells[x].Value == (int)dataGridView1.Rows[y].Cells[x].Value) || ((int)dataGridView1.Rows[y - 1].Cells[x].Value * (int)dataGridView1.Rows[y].Cells[x].Value == 0))
+                    {
+                        dataGridView1.Rows[y - 1].Cells[x].Value = (int)dataGridView1.Rows[y].Cells[x].Value + (int)dataGridView1.Rows[y - 1].Cells[x].Value;
+                        dataGridView1.Rows[y].Cells[x].Value = 0;
+                    }
+
+
+
+                }
+
+
+            }
+
+        }
+
+        private void downShift(int column, int row)
+        {
+
+
+
+            for (int y = row; y < 3; y++)
+            {
+                for (int x = column; x < 4; x++)
+                {
+
+                    if (((int)dataGridView1.Rows[y + 1].Cells[x].Value == (int)dataGridView1.Rows[y].Cells[x].Value) || ((int)dataGridView1.Rows[y + 1].Cells[x].Value * (int)dataGridView1.Rows[y].Cells[x].Value == 0)) 
+                    {
+                        dataGridView1.Rows[y + 1].Cells[x].Value = (int)dataGridView1.Rows[y].Cells[x].Value + (int)dataGridView1.Rows[y + 1].Cells[x].Value;
+                        dataGridView1.Rows[y].Cells[x].Value = 0;
+                    }
+
+
+                }
+
+
+            }
+
+        }
+
+        private void rightShift(int column, int row)
+        {
+
+            for (int y = row; y < 4; y++)
+            {
+                for (int x = column; x < 3; x++)
+                {
+
+                    if (((int)dataGridView1.Rows[y].Cells[x + 1].Value == (int)dataGridView1.Rows[y].Cells[x].Value) || ((int)dataGridView1.Rows[y].Cells[x + 1].Value * (int)dataGridView1.Rows[y].Cells[x].Value == 0))
+                    {
+                        dataGridView1.Rows[y].Cells[x + 1].Value = (int)dataGridView1.Rows[y].Cells[x].Value + (int)dataGridView1.Rows[y].Cells[x + 1].Value;
+                        dataGridView1.Rows[y].Cells[x].Value = 0;
+                    }
+
+
+
+
+
+
+
+
+                }
+            }
+        }
+
+        private void newValue() 
+        {
+
+            Random rand = new Random((int)DateTime.Now.Ticks);
+            for (int x = 0; x < rand.Next(1, 3); x++)
+            {
+                int i = rand.Next(0, 4);
+                int j = rand.Next(0, 4);
+                if ((int)dataGridView1.Rows[i].Cells[j].Value == 0)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = 2;
+                }
+
+
+
+            }
+
+
+
+        }
+
+
+
+    }
+}
